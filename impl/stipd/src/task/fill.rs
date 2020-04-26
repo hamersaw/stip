edb0c33d45ddd2171ecabd5fbdadf2427861be0a
Daniel Rammer <hamersaw@protonmail.com>
2020-04-26 08:26:30 -0600

    refactored FillTask.

--- impl/stipd/src/task/fill.rs
+++ impl/stipd/src/task/fill.rs
@@ -4,11 +4,11 @@ use gdal::raster::{Dataset, Driver};
 use crate::image::{RAW_DATASET, FILLED_DATASET, ImageManager, ImageMetadata};
 use crate::task::{Task, TaskHandle, TaskStatus};
 
-use std::cmp::Ordering;
+use std::cmp::Ordering as CmpOrdering;
 use std::error::Error;
 use std::path::Path;
 use std::sync::{Arc, RwLock};
-use std::sync::atomic::{AtomicU32, Ordering as AtomicOrdering};
+use std::sync::atomic::{AtomicU32, Ordering};
 
 pub struct FillTask {
     band: String,
@@ -46,17 +46,17 @@ impl Task for FillTask {
         // order by platform, geohash, band
         filter_images.sort_by(|a, b| {
             let platform_cmp = a.platform.cmp(&b.platform);
-            if platform_cmp != Ordering::Equal {
+            if platform_cmp != CmpOrdering::Equal {
                 return platform_cmp;
             }
 
             let geohash_cmp = a.geohash.cmp(&b.geohash);
-            if geohash_cmp != Ordering::Equal {
+            if geohash_cmp != CmpOrdering::Equal {
                 return geohash_cmp;
             }
 
             let band_cmp = a.band.cmp(&b.band);
-            if band_cmp != Ordering::Equal {
+            if band_cmp != CmpOrdering::Equal {
                 return band_cmp;
             }
 
@@ -111,9 +111,24 @@ impl Task for FillTask {
             let receiver_clone = receiver.clone();
 
             let join_handle = std::thread::spawn(move || {
-                if let Err(e) = worker_thread(image_manager,
-                        items_completed, items_skipped, receiver_clone) {
-                    panic!("worker thread failure: {}", e);
+                // iterate over records
+                loop {
+                    // fetch next record
+                    let record: Vec<ImageMetadata> = 
+                            match receiver_clone.recv() {
+                        Ok(record) => record,
+                        Err(_) => break,
+                    };
+
+                    // process record
+                    match process(&image_manager, &record) {
+                        Ok(_) => items_completed.fetch_add(1, Ordering::SeqCst),
+                        Err(e) => {
+                            warn!("skipping record '{:?}': {}",
+                                &record, e);
+                            items_skipped.fetch_add(1, Ordering::SeqCst)
+                        },
+                    };
                 }
             });
 
@@ -171,89 +186,76 @@ impl Task for FillTask {
     }
 }
 
-fn worker_thread(image_manager: Arc<ImageManager>,
-        items_completed: Arc<AtomicU32>, items_skipped: Arc<AtomicU32>,
-        receiver: Receiver<Vec<ImageMetadata>>) -> Result<(), Box<dyn Error>> {
-    // iterate over records
-    loop {
-        let record: Vec<ImageMetadata> = match receiver.recv() {
-            Ok(record) => record,
-            Err(_) => break,
-        };
+fn process(image_manager: &Arc<ImageManager>,
+        record: &Vec<ImageMetadata>) -> Result<(), Box<dyn Error>> {
+    // check if path exists
+    let path = Path::new(&record[0].path);
+    if !path.exists() {
+        return Err(format!("image path '{}' does not exist",
+            path.to_string_lossy()).into());
+    }
+
+    // open image - TODO error
+    let dataset = Dataset::open(&path).unwrap();
+
+    // read dataset rasterbands
+    let mut rasters = Vec::new();
+    for i in 0..dataset.count() {
+        let raster = dataset.read_full_raster_as::<u8>(i+1).unwrap();
+        rasters.push(raster);
+    }
+
+    for i in 1..record.len() {
+        let image = &record[i];
 
         // check if path exists
-        let path = Path::new(&record[0].path);
-        if !path.exists() {
-            // increment items skipped counter
-            items_skipped.fetch_add(1, AtomicOrdering::SeqCst);
+        let fill_path = Path::new(&image.path);
+        if !fill_path.exists() {
+            // TODO - log
             continue;
         }
 
-        // open image - TODO error
-        let dataset = Dataset::open(&path).unwrap();
- 
-        // read dataset rasterbands
-        let mut rasters = Vec::new();
-        for i in 0..dataset.count() {
-            let raster = dataset.read_full_raster_as::<u8>(i+1).unwrap();
-            rasters.push(raster);
+        // open fill image - TODO  error
+        let fill_dataset = Dataset::open(&fill_path).unwrap();
+
+        // read fill dataset rasterbands
+        let mut fill_rasters = Vec::new();
+        for i in 0..fill_dataset.count() {
+            let fill_raster = fill_dataset
+                .read_full_raster_as::<u8>(i+1).unwrap();
+            fill_rasters.push(fill_raster);
         }
 
-        for i in 1..record.len() {
-            let image = &record[i];
+        // fill rasterband
+        st_image::fill(&mut rasters, &fill_rasters)?;
+    }
 
-            // check if path exists
-            let fill_path = Path::new(&image.path);
-            if !fill_path.exists() {
-                // TODO - log
-                continue;
-            }
+    // open memory dataset
+    let (width, height) = dataset.size();
+    let driver = Driver::get("Mem").unwrap();
+    let mem_dataset = driver.create("unreachable", width as isize,
+        height as isize, rasters.len() as isize).unwrap();
 
-            // open fill image - TODO  error
-            let fill_dataset = Dataset::open(&fill_path).unwrap();
- 
-            // read fill dataset rasterbands
-            let mut fill_rasters = Vec::new();
-            for i in 0..fill_dataset.count() {
-                let fill_raster = fill_dataset
-                    .read_full_raster_as::<u8>(i+1).unwrap();
-                fill_rasters.push(fill_raster);
-            }
+    mem_dataset.set_geo_transform(
+        &dataset.geo_transform().unwrap()).unwrap();
+    mem_dataset.set_projection(
+        &dataset.projection()).unwrap();
 
-            // fill rasterband
-            st_image::fill(&mut rasters, &fill_rasters)?;
-        }
- 
-        // open memory dataset
-        let (width, height) = dataset.size();
-        let driver = Driver::get("Mem").unwrap();
-        let mem_dataset = driver.create("unreachable", width as isize,
-            height as isize, rasters.len() as isize).unwrap();
+    // set rasterbands - TODO error
+    for (i, raster) in rasters.iter().enumerate() {
+        mem_dataset.write_raster((i + 1) as isize,
+            (0, 0), (width, height), &raster).unwrap();
+    }
 
-        mem_dataset.set_geo_transform(
-            &dataset.geo_transform().unwrap()).unwrap();
-        mem_dataset.set_projection(
-            &dataset.projection()).unwrap();
+    // write mem_dataset - TODO error
+    let image = &record[0];
+    let tile_id = &path.file_name().unwrap().to_string_lossy();
+    let coverage = st_image::coverage(&mem_dataset).unwrap();
 
-        // set rasterbands - TODO error
-        for (i, raster) in rasters.iter().enumerate() {
-            mem_dataset.write_raster((i + 1) as isize,
-                (0, 0), (width, height), &raster).unwrap();
-        }
-
-        // write mem_dataset - TODO error
-        let image = &record[0];
-        let tile_id = &path.file_name().unwrap().to_string_lossy();
-        let coverage = st_image::coverage(&mem_dataset).unwrap();
-
-        if coverage > image.coverage {
-            image_manager.write(&image.platform, &image.geohash, 
-                &image.band, FILLED_DATASET, &tile_id, image.start_date,
-                image.end_date, coverage, &mem_dataset)?;
-        }
-
-        // increment items completed counter
-        items_completed.fetch_add(1, AtomicOrdering::SeqCst);
+    if coverage > image.coverage {
+        image_manager.write(&image.platform, &image.geohash, 
+            &image.band, FILLED_DATASET, &tile_id, image.start_date,
+            image.end_date, coverage, &mem_dataset)?;
     }
 
     Ok(())
