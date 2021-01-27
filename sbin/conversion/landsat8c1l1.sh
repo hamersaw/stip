7ec9b8c0de9bf82061509a6426ae53bf18af9597
Daniel Rammer <hamersaw@protonmail.com>
2021-01-26 23:48:16 -0700

    updated landsat8 conversion script to iterate over arguments - simplifies remote execution

--- sbin/conversion/landsat8c1l1.sh
+++ sbin/conversion/landsat8c1l1.sh
@@ -3,8 +3,8 @@
 # TODO - check for gdalinfo, gdal_translate, and gdal_merge.py
 
 # check arguments
-if [ $# != 1 ]; then
-    echo "usage: $(basename $0) <filename>"
+if (( $# < 1 )); then
+    echo "usage: $(basename $0) FILENAMES..."
     exit
 fi
 
@@ -12,43 +12,45 @@ fi
 tmpdir="/tmp"
 collections=( "1 2 3 4 5 6 7 9 10 11 QA" "8" )
 
-# parse metadata
-tilename=$(basename "$1" | cut -f 1 -d '.')
-directory=$(dirname $1)
+for filename in "$@"; do
+    # parse metadata
+    tilename=$(basename "$filename" | cut -f 1 -d '.')
+    directory=$(dirname $filename)
 
-datestring=${tilename:17:8}
-if [ -z "$datestring" ]; then
-    echo "failed to identify date string"
-    exit
-fi
+    datestring=${tilename:17:8}
+    if [ -z "$datestring" ]; then
+        echo "failed to identify date string"
+        exit
+    fi
 
-# decompress tar archive
-tar xvf $1 -C $tmpdir > /dev/null
+    # decompress tar archive
+    tar xvf $filename -C $tmpdir > /dev/null
 
-# merge subdataset files for each collection
-count=0
-for collection in "${collections[@]}"; do
-    # compile filenames for merged file
-    mergefilenames=""
-    for subdataset in $collection; do
-        if [ -n "$mergefilenames" ]; then
-            mergefilenames="$mergefilenames "
-        fi
+    # merge subdataset files for each collection
+    count=0
+    for collection in "${collections[@]}"; do
+        # compile filenames for merged file
+        mergefilenames=""
+        for subdataset in $collection; do
+            if [ -n "$mergefilenames" ]; then
+                mergefilenames="$mergefilenames "
+            fi
 
-        mergefilenames+="$tmpdir/$tilename\_B$subdataset.TIF"
+            mergefilenames+="$tmpdir/$tilename\_B$subdataset.TIF"
+        done
+
+        # merge subdatasets
+        filename="$directory/$tilename-$count.tif"
+        gdal_merge.py -o "$filename" -separate $mergefilenames >/dev/null
+
+        # set metadata
+        gdal_edit.py -mo "PLATFORM=Landsat8C1L1" -mo "SUBDATASET=$count" \
+            -mo "TILE=$tilename" -mo "TIMESTAMP=$datestring" "$filename"
+
+        # increment subdataset count
+        count=$(( $count + 1 ))
     done
 
-    # merge subdatasets
-    filename="$directory/$tilename-$count.tif"
-    gdal_merge.py -o "$filename" -separate $mergefilenames >/dev/null
-
-    # set metadata
-    gdal_edit.py -mo "PLATFORM=Landsat8C1L1" -mo "SUBDATASET=$count" \
-        -mo "TILE=$tilename" -mo "TIMESTAMP=$datestring" "$filename"
-
-    # increment subdataset count
-    count=$(( $count + 1 ))
+    # cleanup
+    rm $tmpdir/$tilename*
 done
-
-# cleanup
-rm $tmpdir/$tilename*
